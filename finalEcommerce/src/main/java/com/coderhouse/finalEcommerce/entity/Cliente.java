6418144a72ae1336ff3220cdaa54147a136cd914
MarceloCarabajal <marce.tsgia@gmail.com>
2025-01-20 00:46:59 -0300

    creacion de documentación con Swagger

--- finalEcommerce/src/main/java/com/coderhouse/finalEcommerce/entity/Cliente.java
+++ finalEcommerce/src/main/java/com/coderhouse/finalEcommerce/entity/Cliente.java
@@ -23,28 +23,28 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Autoincremental
-    @Schema(description = "ID del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "1")
+    @Schema(description = "ID del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private Long Id;
 
-    @Schema(description = "Nombre del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "Marcelo")
+    @Schema(description = "Nombre del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "Marcelo")
     private String nombre;
 
-    @Schema(description = "Apellido del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "Carabajal")
+    @Schema(description = "Apellido del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "Carabajal")
     private String apellido;
 
-    @Schema(description = "Edad del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "34")
+    @Schema(description = "Edad del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "34")
     private int edad;
 
     @Column(unique = true, nullable = false)
-    @Schema(description = "Email del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "example@example.com")
+    @Schema(description = "Email del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "example@example.com")
     private String email;
 
     @Column(unique = true, nullable = false)
-    @Schema(description = "DNI del cliente", requiredMode = Schema.RequiredMode.AUTO, example = "30111222")
+    @Schema(description = "DNI del cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "30111222")
     private int dni;
 
     @Column(name = "num_cliente")
-    @Schema(description = "Numero de cliente", requiredMode = Schema.RequiredMode.AUTO, example = "1")
+    @Schema(description = "Numero de cliente", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private String numCliente;
 
     @ManyToMany(mappedBy = "clientes", fetch = FetchType.EAGER)
